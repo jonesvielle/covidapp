e7a57242710def60780b92229195b8a8db38a957
jones obenobe <obenobejones@gmail.com>
2020-06-25 23:45:23 +0100

    Updates

---
+++ precache-manifest.65f8c5bbc1e270ed8e475df51347069d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12ce9b293d323e280d729ef0b490d22",
+    "url": "/covidapp/index.html"
+  },
+  {
+    "revision": "537fc7567903aae8085a",
+    "url": "/covidapp/static/css/main.ff16af3e.chunk.css"
+  },
+  {
+    "revision": "439a098ce2882af8cdc6",
+    "url": "/covidapp/static/js/2.5c8e523d.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/covidapp/static/js/2.5c8e523d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537fc7567903aae8085a",
+    "url": "/covidapp/static/js/main.1d50b314.chunk.js"
+  },
+  {
+    "revision": "d54f1368342ae71f11f9",
+    "url": "/covidapp/static/js/runtime-main.76d2cd2f.js"
+  },
+  {
+    "revision": "00c7fc7fb2f170ad6c5bf97249bd7900",
+    "url": "/covidapp/static/media/MuseoModerno-VariableFont_wght.00c7fc7f.ttf"
+  },
+  {
+    "revision": "708d3401debadb9436f19fba7273e273",
+    "url": "/covidapp/static/media/aka.708d3401.jpg"
+  },
+  {
+    "revision": "e5ac49b58fda417078336d4e619fd6fd",
+    "url": "/covidapp/static/media/eyes.e5ac49b5.png"
+  },
+  {
+    "revision": "9983546377120b52c182b5e824fb33c4",
+    "url": "/covidapp/static/media/jones.99835463.jpg"
+  },
+  {
+    "revision": "3e694613745f3910d0179e32593ae10e",
+    "url": "/covidapp/static/media/me.3e694613.png"
+  },
+  {
+    "revision": "38fc168e44a92e8d662f50c270c2f5a5",
+    "url": "/covidapp/static/media/med.38fc168e.svg"
+  },
+  {
+    "revision": "4e30fd456a5516fe27879a1fb03a44f5",
+    "url": "/covidapp/static/media/medi.4e30fd45.jpg"
+  },
+  {
+    "revision": "80f9e2a25dded0bc84d5d663b8274936",
+    "url": "/covidapp/static/media/po4.80f9e2a2.jpg"
+  },
+  {
+    "revision": "58a4da992dc391f2f9fa113d10326255",
+    "url": "/covidapp/static/media/po7.58a4da99.jpg"
+  }
+]);
